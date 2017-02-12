dbcb1443f42ed1d231ee1180923a39ee1bcd143f
Leon Hazard Majestic <GamerCodeXzavier@gmail.com>
2017-02-12 08:02:18 +0800

    @Create

---
+++ src/IFC/Core.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace IFC;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+
+class Core extends PluginBase implements Listener {
+
+public function onLoad() {
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+?>
